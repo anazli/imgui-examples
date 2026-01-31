ba6695aefb6d4010807603c81545f7e3c6277d04
anazli <andreas221b@gmail.com>
2026-01-31 11:04:41 +0100

    first ImGui demo example

--- example1/CMakeProject1/CMakeProject1/CMakeProject1.cpp
+++ example1/CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -10,4 +10,53 @@
 
 using namespace std;
 
-int main() { return 0; }
+int main() {
+  if (!glfwInit()) {
+    return 1;
+  }
+
+  auto window = glfwCreateWindow(800, 600, "ImGui Example - GLFW/OpenGl",
+                                 nullptr, nullptr);
+  if (!window) {
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  auto io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 130");
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::ShowDemoWindow();
+
+    ImGui::Render();
+    int w, h;
+    glfwGetFramebufferSize(window, &w, &h);
+    glViewport(0, 0, w, h);
+    glClearColor(0.45f, 0.55f, 0.6f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
+  }
+
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
